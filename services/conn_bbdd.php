a6952d4bf534febaf3fbd70d5955d10b352aa01b
Moisés <sgastur@gmail.com>
2025-12-17 16:38:52 +0100

    Update conn

--- services/conn_bbdd.php
+++ services/conn_bbdd.php
@@ -1,23 +1,21 @@
 <?php
-try {
-    $host = "ikw4cos008ksg4w4g04cso4k";
-    $db   = "itec_elevacion";
-    $user = "mysql";
-    $pass = "LELGRzf6r3yJoSzmYzvcgZK59KjxCYdGgc6q4dTjXLbxbtTlW2xKbHa6WgwTetXf";
-
-    $link = new PDO(
-        "mysql:host=$host;dbname=$db;charset=utf8mb4",
-        $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false
-        ]
+try{
+    $link = mysqli_connect(
+        "ikw4cos008ksg4w4g04cso4k", 
+        "mysql",
+        "LELGRzf6r3yJoSzmYzvcgZK59KjxCYdGgc6q4dTjXLbxbtTlW2xKbHa6WgwTetXf", 
+        "itec_elevacion"
     );
 
-} catch (PDOException $e) {
-    http_response_code(500);
-    echo "Error de conexión a la base de datos";
-    exit;
+    if (!$link) {
+        die("Conexión fallida: " . mysqli_connect_error());
+    }
+
+    // Charset utf8
+    mysqli_set_charset($link, "utf8");
+} catch (Exception $e) {
+    echo 'Excepción capturada: ',  $e->getMessage(), "\n";
 }
+    
+?>
+
