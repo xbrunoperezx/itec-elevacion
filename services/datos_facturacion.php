6bb2ab031e07ed15dbcebb1b299087a40fbb68b2
Moisés <sgastur@gmail.com>
2026-01-02 10:51:32 +0100

    features/datos_facturacion service

--- services/datos_facturacion.php
+++ services/datos_facturacion.php
@@ -13,6 +13,29 @@ if (!$link) {
     die("Conexión fallida: " . mysqli_connect_error());
 }
 
+// Consulta para obtener la información de los formas de pago
+$formaspago_sql = "SELECT * FROM formas_pago";
+$formaspago_result = mysqli_query($link, $formaspago_sql);
+
+// Inicialización de un array para almacenar los datos de los formas de pago
+$formaspago = array();
+// Bucle a través de cada fila de resultados de formas de pago y almacenamiento de datos en el array
+while ($row = mysqli_fetch_assoc($formaspago_result)) {
+    $formaspago[$row["id"]] = $row["forma_pago"];
+}
+
+
+// Consulta para obtener la información de las tarifas
+$tarifas_sql = "SELECT * FROM tarifas";
+$tarifas_result = mysqli_query($link, $tarifas_sql);
+
+// Inicialización de un array para almacenar los datos de las tarifas
+$tarifas = array();
+// Bucle a través de cada fila de resultados de tarifas y almacenamiento de datos en el array
+while ($row = mysqli_fetch_assoc($tarifas_result)) {
+    $tarifas[$row["id"]] = $row["tarifa"];
+}
+
 if($id!=""){
     // Define la consulta SQL
     $sql = "SELECT * FROM datos_facturacion WHERE id=$id ORDER BY id ASC";
@@ -24,6 +47,16 @@ if($id!=""){
 
     // Bucle a través de cada fila de resultados y almacenamiento de datos en el array
     while ($row = mysqli_fetch_assoc($result)) {
+        if (isset($formaspago[$row['id_forma_pago']])) {
+        $row['forma_pago'] = $formaspago[$row['id_forma_pago']];
+        }else{
+        $row['forma_pago'] = "-";
+        }
+        if (isset($tarifas[$row['id_tarifas']])) {
+        $row['tarifa'] = $tarifas[$row['id_tarifas']];
+        }else{
+        $row['tarifa'] = "-";
+        }
         $resultados[] = $row;
     }
 
