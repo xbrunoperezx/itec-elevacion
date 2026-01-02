adcaece2775bbdcc1f2ff5b9d11c6c0732dd1c05
Moisés <sgastur@gmail.com>
2026-01-02 12:06:32 +0100

    upgrade/tabla_cli_fac

--- js/func_clientes.js
+++ js/func_clientes.js
@@ -131,8 +131,8 @@ var readDatosFacturacion = function(id){
 				tr += '(' + row['fis_provincia'] + ')<br>';
 				tr += 'Observaciones: ' + row['observaciones'] + '<br>&nbsp;';
 				tr += '</td>';
-				tr += '<td>' + row['tarifa'] + '</td>';
-				tr += '<td>' + row['forma_pago'] + '</td>';
+				tr += '<td>' + row['tarifa'] + '<br>' + row['forma_pago'] + '</td>';
+				tr += '<td>' + row['num_cuenta'] + '</td>';
 				tr += '<td><a seccion="cli" tipo="frm_edit_fac" data-id="' + row['id'] + '" class="editar_cli_fac btn-floating btn-small waves-effect waves-light red" title="Eliminar datos"><i class="material-icons">delete_forever</i></a></td>';
 				tr += '</tr>';
 				$("#table_cli_fact").append(tr);
