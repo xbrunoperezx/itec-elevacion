e13b6bd54b12d37211f9d713b08a36a7ca76b65c
mitnick <brunovega1985@gmail.com>
2026-01-09 20:15:16 +0100

    Realizando cambios por conflictos

--- services/clientes_delete.php
+++ services/clientes_delete.php
@@ -1,21 +1,27 @@
 <?php
 include("conn_bbdd.php");
 
-$response= [
+// respuesta por defecto
+$response[
     "success" => false,
     "message" => "Error desconocido"
 ];
 
+// comprobamos que nos llega el ID
 if (!isset($_POST['id'])){
     $response= ["message"]= "ID no recibido";
     echo json_encode($response);
     exit;
 }
 
-$id= intval($_POST['id']); // seguridad basica
+//convertimos el ID a  nuemro entero (Seguridad)
+$id= intval($_POST['id']); // seguridad basica evita injeccion SQL y grantiza que sea un numero el que busca
 
+// la consulta query donde se borra el cliente
 $sql= "DELETE FROM clientes WHERE id= $id";
 
+
+// ejecutamos al consulta query
 if(mysqli_query($link, $sql)) {
     $response= ["success"] = true;
     $response= ["message"] = "Cliente eliminado correctamente";
@@ -23,8 +29,11 @@ if(mysqli_query($link, $sql)) {
     $response= ["messagge"] ="Error al eliminar cliente";
 }
 
+// cerramos la conexion con la DB
 mysqli_close($link);
 
+
+//Respondemos al frontend
 echo json_encode($response);
 
 
