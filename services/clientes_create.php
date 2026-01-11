a521294307a8f70d9565aa657b38c784933cc8a1
mitnick <brunovega1985@gmail.com>
2026-01-11 19:44:00 +0100

    creamos archvio de back de craear clientes, con las seguridaddes basicas validaciones  y su consulta

---
+++ services/clientes_create.php
@@ -0,0 +1,47 @@
+<?php
+include("conn_bbdd.php");
+
+$response = array (
+    "success" => false,
+    "message" => "Error de desconocida"
+);
+
+//validacion 
+if (
+    empty($_POST['nombre']) ||
+    empty($_POST['direccion']) ||
+    empty($_POST['localidad']) ||
+    empty($_POST['cp'])
+) {
+    $response['message'] = "Todos los campos son obligatorios",
+    echo json_encode($response);
+    exit;
+}
+
+//seguridad basica 
+$nombre= mysqli_real_escape_string($link, $_POST['nombre']);
+$direccion= mysqli_real_escape_string($link, $_POST['direccion']);
+$localidad= mysqli_real_escape_string($link, $_POST['localidad']);
+$cp= mysqli_real_escape_string($link, $_POST['cp']);
+
+
+//la consulta de INSERT para nuevo cliente
+$sql= "
+    INSERT INTO clientes(nombre, direccion, localidad, cp)
+    VALUES('$nombre', '$direccion', '$localidad', '$cp')
+
+";
+
+//ejecutamos la query/consulta  si todo va bien el cliente se actualiza y js recibe success: true si no da el error
+if(mysqli_query($link, $sql)){
+    $response['success']= true;
+    $response['message']= "Cliente creado correctamente";
+} else {
+    $reponse['message']= "Error al crear cliente";
+}
+
+//cerramos conexion
+mysqli_close($link);
+
+//respondemos al front
+echo json_encode($response);
