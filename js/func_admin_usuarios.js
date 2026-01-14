8a65a4a92958c577be1e6d01d977751ef79844f0
Moisés <sgastur@gmail.com>
2026-01-14 10:05:01 +0100

    Cambio order

--- js/func_admin_usuarios.js
+++ js/func_admin_usuarios.js
@@ -82,8 +82,8 @@ function readUsuarios(){
       tr += "<td>" + nombreUsr + "</td>";
       tr += "<td class='ancho200'><span class='main-text'>" + usuario + "</span></td>";
       tr += "<td class='ancho200'>" + email + "</td>";
+      tr += "<td class='ancho50'>" + tipoIcon + "</td>";
       tr += "<td class='ancho200'>" + puesto + "</td>";
-      tr += "<td class='ancho100'>" + tipoIcon + "</td>";
       tr += "<td class='ancho150'><a class='btn-floating btn-small waves-effect waves-light black' title='Enviar email' href='mailto:"+email+"'><i class='material-icons'>email</i></a>&nbsp;";
       if(Object.keys(equipos).length > 0){
         tr += "<a class='btn-floating btn-small waves-effect waves-light red' title='";
