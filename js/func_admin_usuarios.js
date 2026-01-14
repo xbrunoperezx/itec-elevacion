e52d34215359a8fc1956d1c172be899d9baa5c72
Moisés <sgastur@gmail.com>
2026-01-14 12:10:02 +0100

    Añadir guardar usuario

--- js/func_admin_usuarios.js
+++ js/func_admin_usuarios.js
@@ -1,6 +1,8 @@
 // Funciones CRUD para administración de usuarios (cliente-side)
 // Usa jQuery para llamadas AJAX a services/usuarios.php
 
+var MD5 = function(d){var r = M(V(Y(X(d),8*d.length)));return r.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_}
+
 var UsuariosAPI = (function(){
   function request(data){
     return $.ajax({
@@ -154,6 +156,84 @@ $(function(){
   });
 });
 
+// funcion guardar el usuario (nuevo/editar)
+var saveUsuario = function() {
+
+  // indicar estado en el modal de confirmación
+  $('#confirm-message').text("...guardando los cambios...");
+
+  var id = $('#id_usr').val();
+  var user = ($('#user_usr').val() || '').trim();
+  var password = ($('#password_usr').val() || '');
+  var name = ($('#name_usr').val() || '').trim();
+  var email = ($('#email_usr').val() || '').trim();
+  var extension = ($('#extension_usr').val() || '').trim();
+  var pphone = ($('#pphone_usr').val() || '').trim();
+  var oficina = ($('#oficina_usr').val() || '').trim();
+  var puesto = ($('#puesto_usr').val() || '').trim();
+  var tipo = ($('#tipo_usr').val() || '').trim();
+  var abrev = ($('#abrev_usr').val() || '').trim();
+  var equipos = ($('#equipos_usr').val() || '').trim();
+
+  var usuario = {
+    user: user,
+    password: password,
+    name: name,
+    email: email,
+    extension: extension,
+    pphone: pphone,
+    oficina: oficina,
+    puesto: puesto,
+    tipo: tipo,
+    abrev: abrev,
+    equipos: equipos
+  };
+
+  if (typeof id !== 'undefined' && id !== null && String(id).trim() !== '') {
+    usuario.id = id;
+  }
+
+  $.ajax({
+    url: 'services/usuarios_save.php',
+    type: 'POST',
+    data: usuario,
+    success: function(data){
+      if (typeof data === 'string' && data.trim() === 'OK'){
+        $('#modal_confirm').modal('close');
+        $('#modal_usu').modal('close');
+        $('#filtrar_usuarios').click();
+        return;
+      }
+      var serverMsg = (typeof data === 'string') ? data : JSON.stringify(data);
+      modalError('ERROR', 'Error al guardar usuario: ' + serverMsg, false, 'Cerrar', 'error', function(){
+        $('#modal_usu').modal('close');
+      });
+    },
+    error: function(xhr, status, error){
+      var msg = '';
+      if(xhr && xhr.responseText){
+        msg = xhr.status + ' ' + (xhr.statusText || '') + ': ' + xhr.responseText;
+      } else {
+        msg = status + ' - ' + error;
+      }
+      modalError('ERROR', 'Error en la petición al guardar usuario. ' + msg, false, 'Cerrar', 'error', function(){
+        $('#modal_usu').modal('close');
+      });
+    }
+  });
+}; // end saveUsuario()
+
+
+// Click en guardar (botón común #cli_save). Solo actuar si el modal usuario está abierto
+$(document.body).on('click', '#cli_save', function(){
+  if($('#modal_usu').length && $('#modal_usu').is(':visible')){
+    modalConfirm("Guardar cambios en usuario", "¿Estás seguro de que quieres guardar los cambios?\n\n", false, "Guardar", "Cancelar", "save", "clear", function(){
+      saveUsuario();
+    }, function(){
+      // cancel
+    });
+  }
+});
 var openUsuario = function(seccion, cual, id){
   if(cual === 'frm_editusu'){
     var totalParams = { filtro_id: id };
@@ -176,7 +256,7 @@ var openUsuario = function(seccion, cual, id){
             '</div>' +
             '<div class="input-field anchoFrm4 inline">' +
               '<input type="password" id="password_usr" name="password" value="" autocomplete="off">' +
-              '<label for="password_usr">Contraseña</label>' +
+              '<label for="password_usr">Cambiar contraseña</label>' +
             '</div>' +
           '</div>' +
           '<div class="input-field">' +
@@ -245,49 +325,55 @@ var openUsuario = function(seccion, cual, id){
     $("#modal_"+seccion).find(".modal_txt_btn_right").html("<i class='material-icons left'>exit_to_app</i>Salir");
 
     var frm = '<form id="usuario_frm_nuevo">' +
-      '<div class="input-field">' +
-        '<input type="text" id="user_usr" name="user" value="">' +
-        '<label for="user_usr">Usuario</label>' +
+      '<div class="row">' +
+        '<div class="input-field anchoFrm4 left">' +
+          '<input type="text" id="user_usr" name="user" value="" autocomplete="off">' +
+          '<label for="user_usr">Usuario</label>' +
+        '</div>' +
+        '<div class="input-field anchoFrm4 inline">' +
+          '<input type="password" id="password_usr" name="password" value="" autocomplete="off">' +
+          '<label for="password_usr">Contraseña</label>' +
+        '</div>' +
       '</div>' +
       '<div class="input-field">' +
-        '<input type="password" id="password_usr" name="password" value="">' +
-        '<label for="password_usr">Contraseña</label>' +
-      '</div>' +
-      '<div class="input-field">' +
-        '<input type="text" id="name_usr" name="name" value="">' +
+        '<input type="text" id="name_usr" name="name" value="" autocomplete="off">' +
         '<label for="name_usr">Nombre</label>' +
       '</div>' +
       '<div class="input-field">' +
-        '<input type="text" id="email_usr" name="email" value="">' +
+        '<input type="text" id="email_usr" name="email" value="" autocomplete="off">' +
         '<label for="email_usr">Email</label>' +
       '</div>' +
-      '<div class="input-field anchoFrm2">' +
-        '<input type="text" id="extension_usr" name="extension" value="">' +
-        '<label for="extension_usr">Extensión</label>' +
+      '<div class="row">' +
+        '<div class="input-field anchoFrm4 left">' +
+          '<input type="text" id="pphone_usr" name="pphone" value="" autocomplete="off">' +
+          '<label for="pphone_usr">Teléfono</label>' +
+        '</div>' +
+        '<div class="input-field anchoFrm4 inline">' +
+          '<input type="text" id="extension_usr" name="extension" value="" autocomplete="off">' +
+          '<label for="extension_usr">Extensión</label>' +
+        '</div>' +
+        '<div class="input-field anchoFrm4 inline">' +
+          '<input type="text" id="oficina_usr" name="oficina" value="" autocomplete="off">' +
+          '<label for="oficina_usr">Oficina</label>' +
+        '</div>' +
       '</div>' +
-      '<div class="input-field anchoFrm2">' +
-        '<input type="text" id="pphone_usr" name="pphone" value="">' +
-        '<label for="pphone_usr">Teléfono</label>' +
-      '</div>' +
-      '<div class="input-field">' +
-        '<input type="text" id="oficina_usr" name="oficina" value="">' +
-        '<label for="oficina_usr">Oficina</label>' +
-      '</div>' +
-      '<div class="input-field">' +
-        '<input type="text" id="puesto_usr" name="puesto" value="">' +
-        '<label for="puesto_usr">Puesto</label>' +
-      '</div>' +
-      '<div class="input-field">' +
-        '<select id="tipo_usr" name="tipo">' +
-          '<option value="admin">admin</option>' +
-          '<option value="inspector">inspector</option>' +
-          '<option value="auxiliar">auxiliar</option>' +
-        '</select>' +
-        '<label for="tipo_usr">Tipo</label>' +
-      '</div>' +
-      '<div class="input-field">' +
-        '<input type="text" id="abrev_usr" name="abrev" value="">' +
-        '<label for="abrev_usr">Abreviatura</label>' +
+      '<div class="row">' +
+        '<div class="input-field anchoFrm4 left">' +
+          '<input type="text" id="puesto_usr" name="puesto" value="" autocomplete="off">' +
+          '<label for="puesto_usr">Puesto</label>' +
+        '</div>' +
+        '<div class="input-field anchoFrm4 inline">' +
+          '<input type="text" id="abrev_usr" name="abrev" value="" autocomplete="off">' +
+          '<label for="abrev_usr">Abreviatura</label>' +
+        '</div>' +
+        '<div class="input-field anchoFrm4 inline">' +
+          '<select id="tipo_usr" name="tipo">' +
+            '<option value="admin">admin</option>' +
+            '<option value="inspector">inspector</option>' +
+            '<option value="auxiliar">auxiliar</option>' +
+          '</select>' +
+          '<label for="tipo_usr">Tipo</label>' +
+        '</div>' +
       '</div>' +
       '<div class="input-field">' +
         '<textarea id="equipos_usr" name="equipos"></textarea>' +
