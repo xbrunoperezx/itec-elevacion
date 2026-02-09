51fbafafc563836f8dd812f4a75359cd2738948a
mitnick <brunovega1985@gmail.com>
2026-02-09 22:13:02 +0100

    corrigiendo respuesta de vencimiento

--- js/bruno_events.js
+++ js/bruno_events.js
@@ -98,7 +98,7 @@ $('#guardar-cambios').on('click', function () {
 
 
     //si no hay error continua con el Ajax
-    
+
     // construimos el objeto que guarda los datos para logo enviarlo por ajax
     // que es lo que espera PHP por $_POST
     let datos = {
@@ -129,7 +129,7 @@ $('#guardar-cambios').on('click', function () {
                 cliente.localidad = datos.localidad;
                 cliente.id_mantenedor = datos.id_mantenedor;
                 cliente.cp = datos.cp;
-                cliente.vencimiento = datos.vencimiento;
+                cliente.vencimiento = response.datos.vencimiento;
                 cliente.contratada = datos.contratada;
 
                 //con esto nos devuelve el nombre y no la ID(busca en el array global usando la id y nos devuelve su nombre)
