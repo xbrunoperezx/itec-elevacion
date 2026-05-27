2afefa2ddf1ff5ffb4094b1963bde9a7262da067
Moisés <sgastur@gmail.com>
2026-05-27 16:10:38 +0200

    Refactor modal_pri tab11 layout; adjust flex properties and improve structure for better responsiveness

--- js/func_informes.js
+++ js/func_informes.js
@@ -2663,15 +2663,13 @@ var openInforme = function(seccion, cual, id){
 						'<div id="tab10_pri" class="col s12">' + 
 						'</div>' +	
 						'<div id="tab11_pri" class="col s12">' + 
-						'<div class="row">' +
-						  '<div class="col s12">' +
-						    informeStepperHtml +
-						  '</div>' +
-						'</div>' +
-						'<div class="row">' +
+						'<div class="row tab11-stepper-row">' +
 						  '<div class="input-field col s4">' +
 						    '<input type="text" id="id_bbdd" name="id_bbdd" value="' + item.id + '" disabled>' +
 						    '<label for="id_bbdd" class="active">ID BBDD</label>' +
+						  '</div>' +
+						  '<div class="col s8 tab11-stepper-col">' +
+						    informeStepperHtml +
 						  '</div>' +
 						'</div>' +
 						'</div>' +	
